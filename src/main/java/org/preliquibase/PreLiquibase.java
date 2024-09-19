543df15b1f8e0b7c65846095d9ae606dcadd84a3
v3lt3r <79567861+Vladislav-Trubicyn@users.noreply.github.com>
2024-09-19 09:24:12 +0400

    init

---
+++ src/main/java/org/preliquibase/PreLiquibase.java
@@ -0,0 +1,63 @@
+package org.preliquibase;
+
+import io.micrometer.common.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class PreLiquibase implements BeanPostProcessor {
+    @Value("${spring.liquibase.url:}")
+    private String jdbcUrl;
+    @Value("${spring.liquibase.driver-class-name:}")
+    private String driverClass;
+    @Value("${spring.liquibase.user:}")
+    private String user;
+    @Value("${spring.liquibase.password:}")
+    private String password;
+
+    private boolean isDone;
+
+    private Logger log = LoggerFactory.getLogger(PreLiquibase.class);
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (!isDone) {
+            String[] splitJdbcUrl = jdbcUrl.split("/");
+
+            if(splitJdbcUrl.length < 4) {
+                throw new RuntimeException("Unable to parse URL " + jdbcUrl);
+            }
+
+            String jdbcUrlWithoutDatabase = String.format("%s//%s/", splitJdbcUrl[0], splitJdbcUrl[2]);
+            String dataBaseName = splitJdbcUrl[3];
+            DriverManagerDataSource dataSource = new DriverManagerDataSource(jdbcUrlWithoutDatabase, user, password);
+            if(StringUtils.isNotEmpty(driverClass)) {
+                dataSource.setDriverClassName(driverClass);
+            }
+
+            try (Connection conn = dataSource.getConnection();
+                 Statement statement = conn.createStatement()) {
+                log.info("Going to create DB '{}' if not exists.", dataBaseName);
+                statement.execute("create database " + dataBaseName);
+            } catch (SQLException e) {
+                if(!e.getSQLState().equals("42P04")){
+                    throw new RuntimeException("Failed to create database '" + dataBaseName + "'", e);
+                }
+            }
+            isDone = true;
+        }
+        return bean;
+    }
+}
---
+++ src/main/java/org/preliquibase/PreLiquibaseAutoConfiguration.java
@@ -0,0 +1,21 @@
+package org.preliquibase;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+
+@AutoConfiguration
+@EnableAutoConfiguration
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@ComponentScan
+public class PreLiquibaseAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    PreLiquibase getPreLiquibaseConfig() {
+        return new PreLiquibase();
+    }
+}
